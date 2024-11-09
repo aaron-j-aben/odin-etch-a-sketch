9adbe8da59012682f775f5f791e354ce91436699
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-08 20:53:35 -0500

    Link css and js files to the html, set up function declarations for grid generation

---
+++ etch_a_sketch.js
@@ -0,0 +1,28 @@
+/**
+ * Etch-A-Sketch
+ * By Aaron J. Aben
+ * 
+ * Generates an nxn grid of squares in which the squares change color
+ * when hovered on.
+ */
+
+const GRID_PX = 1000;
+
+// Represent grid as column flexbox of row flexboxes
+
+/**
+ * Generates a single square div based on the grid dimensions and the number of
+ * squares required
+ * @param {number} n 
+ */
+function genSquare(n) {
+
+}
+
+function genRow(n) {
+
+}
+
+function genGrid(n) {
+
+}
